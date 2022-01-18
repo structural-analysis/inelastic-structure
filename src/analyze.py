cd361b3a91b8e81e84cbe1e01b882c233452fcea
Standing Warrior <thelaststandingwarriors@gmail.com>
2022-01-18 16:29:18 +0330

    incomplete 3

--- src/analyze.py
+++ src/analyze.py
@@ -15,4 +15,19 @@ for i, element in enumerate(structure.elements):
     p0[2 * i] = fs[0, 2]
     p0[2 * i + 1] = fs[0, 5]
     # disp1 = structure.compute_structure_displacement(structure.)
-    fv = np.zeros((structure.node_n_dof * structure.n_nodes, 1))
+    for section in range(2):
+        fv = np.zeros((structure.node_n_dof * structure.n_nodes, 1))
+        fv = np.matrix(fv)
+        udef_global = element.t.T * element.udef[section].T
+        fv[structure.node_n_dof * element.nodes[0].num] = udef_global[0]
+        fv[structure.node_n_dof * element.nodes[0].num + 1] = udef_global[1]
+        fv[structure.node_n_dof * element.nodes[0].num + 2] = udef_global[2]
+
+        fv[structure.node_n_dof * element.nodes[1].num] = udef_global[3]
+        fv[structure.node_n_dof * element.nodes[1].num + 1] = udef_global[4]
+        fv[structure.node_n_dof * element.nodes[1].num + 2] = udef_global[5]
+        struc_disp = structure.compute_structure_displacement(fv)
+        elem_disps = structure.get_elements_disps(struc_disp)
+        for elem_disp in elem_disps:
+            fs = element.get_nodal_forces(elem_disp, -element.udef[section].T)
+            
