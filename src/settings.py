c82d9d2d7fb4dc8b0649aa4c05d195b23c2b3158
h4m3d <hamed.valizade43@gmail.com>
2022-10-27 09:35:47 +0330

    revert set example to plate

--- src/settings.py
+++ src/settings.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    example_name: str = "simple_plate"
+    example_name: str = "simple_beam_dynamic"
     computational_zero = 1e-10
     isclose_tolerance = 1e-7
 
