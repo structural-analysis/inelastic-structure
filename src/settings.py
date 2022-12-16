dc7bc4a24b4e14eca8b537fb245d8720f4a9cd27
h4m3d <hamed.valizade43@gmail.com>
2022-12-16 12:43:46 +0330

    change plate examples to rectangular

--- src/settings.py
+++ src/settings.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    example_name: str = "simple_plate_4mesh_elastic"
+    example_name: str = "simple_plate_elastic_1mesh"
     computational_zero = 1e-10
     isclose_tolerance = 1e-7
     examples_to_test = [
