8254ace4b4bc3962e37b5d45204e1db1e2bfa35a
Standing Warrior <thelaststandingwarriors@gmail.com>
2023-06-11 13:03:09 +0430

    test: example simple_beam_static_elastic added

--- src/settings.py
+++ src/settings.py
@@ -2,13 +2,14 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    example_name: str = "simple_beam"
+    example_name: str = "simple_beam_static_elastic"
     computational_zero = 1e-10
     isclose_tolerance = 1e-7
     examples_to_test = [
+        "simple_beam_static_elastic",
+        "simple_beam_static_inelastic",
         "3story_perfect",
         "3story_softening",
-        "simple_beam",
         "skew_beams",
         "tripod_corner",
         "tripod_unload",
