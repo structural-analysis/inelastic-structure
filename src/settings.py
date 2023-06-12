0e47512ba359120839e7ae41057192f70ce3aafe
Standing Warrior <thelaststandingwarriors@gmail.com>
2023-06-12 09:34:51 +0430

    fix: transformation for 2d frame corrected.

--- src/settings.py
+++ src/settings.py
@@ -2,12 +2,13 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    example_name: str = "simple_beam_static_elastic"
+    example_name: str = "torre_static_elastic"
     computational_zero = 1e-10
     isclose_tolerance = 1e-7
     examples_to_test = [
         "simple_beam_static_elastic",
         "simple_beam_static_inelastic",
+        "torre_static_elastic",
         "3story_perfect",
         "3story_softening",
         "skew_beams",
