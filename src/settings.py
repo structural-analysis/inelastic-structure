e82019280a3fcb0620abd449005b1de2c5f358ca
Your Name <you@example.com>
2023-06-20 12:40:17 +0430

    convert _ to - in example names, remove extra examples

--- src/settings.py
+++ src/settings.py
@@ -2,26 +2,26 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    example_name: str = "2story_dynamic_inelastic"
+    example_name: str = "2story-dynamic-inelastic"
     computational_zero = 1e-10
     isclose_tolerance = 1e-7
     examples_to_test = [
-        "simple_beam_static_elastic",
-        "simple_beam_static_inelastic",
-        "torre_static_elastic",
-        "skew_beams_static_elastic",
-        "tripod_corner",
-        "tripod_unload",
-        "3story_static_elastic",
-        "3story_static_inelastic_perfect",
-        "3story_static_inelastic_softening",
-        "torre_dynamic_elastic",
-        "simple_beam_dynamic_elastic",
-        "1story_dynamic_elastic",
-        "2story_dynamic_elastic",
-        "simple_beam_dynamic_inelastic_1phase",
-        "1story_dynamic_inelastic_ll1.0_ap400k",
-        "2story_dynamic_inelastic",
+        "simple-beam-static-elastic",
+        "simple-beam-static-inelastic",
+        "torre-static-elastic",
+        "skew-beams-static-elastic",
+        "tripod-corner",
+        "tripod-unload",
+        "3story-static-elastic",
+        "3story-static-inelastic-perfect",
+        "3story-static-inelastic-softening",
+        "torre-dynamic-elastic",
+        "simple-beam-dynamic-elastic",
+        "1story-dynamic-elastic",
+        "2story-dynamic-elastic",
+        "simple-beam-dynamic-inelastic-1phase",
+        "1story-dynamic-inelastic-ll1.0-ap400k",
+        "2story-dynamic-inelastic",
     ]
 
 
