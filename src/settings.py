876c085e94b00a327908e2d66876a2e7799f47a5
Your Name <you@example.com>
2023-06-28 17:54:33 +0430

    added and run wall-4element-elastic-q8r

--- src/settings.py
+++ src/settings.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    example_name: str = "wall-1element-elastic-q8r"
+    example_name: str = "wall-4element-elastic-q8r"
     computational_zero = 1e-10
     isclose_tolerance = 1e-7
     examples_to_test = [
