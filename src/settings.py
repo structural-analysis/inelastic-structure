f4e9ad0871f38ee2d8903dfc2c3e35b4201a32c5
Your Name <you@example.com>
2023-07-09 21:20:07 +0430

    add confined and semiconfined plate examples

--- src/settings.py
+++ src/settings.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    example_name: str = "plate-confined-elastic"
+    example_name: str = "plate-semiconfined-inelastic"
     computational_zero = 1e-12
     isclose_tolerance = 1e-7
     examples_to_test = [
