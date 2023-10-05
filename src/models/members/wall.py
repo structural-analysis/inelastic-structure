a2d1a0c11e3bb7e7c49092d26b0d9a7bca4410a6
Standing Warrior <thelaststandingwarriors@gmail.com>
2023-10-05 07:25:45 +0330

    some change

--- src/models/members/wall.py
+++ src/models/members/wall.py
@@ -252,8 +252,8 @@ class WallMember:
             j_det = np.linalg.det(j)
             gauss_point_m = gauss_point.weight * n.T * self.section.material.rho * n * j_det * self.section.geometry.thickness
             m += gauss_point_m
-        diagonal_mass = self.diagonalize_mass(m)
-        return diagonal_mass
+        # diagonal_mass = self.diagonalize_mass(m)
+        return m
 
     def diagonalize_mass(self, m):
         diagonal_m = np.matrix(np.zeros((self.dofs_count, self.dofs_count)))
