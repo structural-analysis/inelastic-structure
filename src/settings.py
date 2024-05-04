f903c58eabf46eb1b60ada2decf732f7661d0552
Your Name <you@example.com>
2024-05-04 16:37:07 +0430

    feat: add and run plate-limit-load-compare-cantil-1element

--- src/settings.py
+++ src/settings.py
@@ -10,7 +10,7 @@ class SiftingType(str, Enum):
 
 
 class Settings(BaseSettings):
-    example_name: str = "plate-limit-load-compare-square-16element"
+    example_name: str = "plate-limit-load-compare-cantil-1element"
     sifting_type: SiftingType = SiftingType.mahini
     computational_zero = 1e-12
     isclose_tolerance = 1e-7
