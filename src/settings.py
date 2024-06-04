e01f6d589c9dc19c5fbeee1796f540e284cf0cd6
Your Name <you@example.com>
2024-06-04 16:03:50 +0330

    feat: add example plate-limit-rec-verify-6x4

--- src/settings.py
+++ src/settings.py
@@ -10,7 +10,7 @@ class SiftingType(str, Enum):
 
 
 class Settings(BaseSettings):
-    example_name: str = "plate-limit-rec-verify-12x8"
+    example_name: str = "plate-limit-rec-verify-6x4"
     sifting_type: SiftingType = SiftingType.mahini
     computational_zero = 1e-12
     isclose_tolerance = 1e-7
