0e2bae674ca87ea789360137a9993dc82cc66b13
Your Name <you@example.com>
2024-10-12 09:34:58 +0330

    chore: clean visualization

--- src/visualization/calc.py
+++
@@ -1,6 +0,0 @@
-sqrt(
-    pow(s1f82_SM.getScalarField(componentLabel="SM1"), 2) + 
-    pow(s1f82_SM.getScalarField(componentLabel="SM2"), 2) -
-    s1f82_SM.getScalarField(componentLabel="SM1") * s1f82_SM.getScalarField(componentLabel="SM2") + 
-    3 * pow(s1f82_SM.getScalarField(componentLabel="SM3"), 2)
-)
