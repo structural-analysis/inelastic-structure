c77d1bfbe989b4c68d9dcb2c8ee57dc171f0486c
Your Name <you@example.com>
2025-01-02 15:03:20 +0330

    feat: add plate-ring example

--- src/settings.py
+++ src/settings.py
@@ -10,7 +10,7 @@ class SiftingType(str, Enum):
 
 
 class Settings(BaseSettings):
-    example_name: str = "3d-2side-dynamic-inelastic"
+    example_name: str = "plate-ring"
     sifting_type: SiftingType = SiftingType.mahini
     computational_zero = 1e-12
     isclose_tolerance = 1e-7
