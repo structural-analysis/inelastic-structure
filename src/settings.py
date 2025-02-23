d19a12f576f6ef4a1650690fee9256522dddbd77
Your Name <you@example.com>
2025-02-23 10:17:25 +0330

    fix: disable monitor disp

--- src/settings.py
+++ src/settings.py
@@ -11,7 +11,7 @@ class SiftingType(str, Enum):
 class Settings(BaseSettings):
     example_name: str = "plate-circular-fine-soft"
     sifting_type: SiftingType = SiftingType.mahini
-    monitor_incremental_disp: bool = True
+    monitor_incremental_disp: bool = False
     controlled_node_for_disp = 37
     controlled_node_dofs_count = 3
     controlled_node_dof_for_disp = 0
